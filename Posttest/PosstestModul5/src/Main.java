7dd7218686f60f9739857a97945847c09f6cbf5a
RianHarahap <rianharahap2303@gmail.com>
2025-03-28 17:22:39 +0700

    Add files via upload

---
+++ Posttest/PosstestModul5/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Alice", 25, 5000.0);
+        emp.display(); 
+        emp.showSalary(); 
+    }
+}
+ 
+    
